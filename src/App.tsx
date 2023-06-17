4ca8be8d726dca9d9093b9f704b655a66c6db1a5
acu8 <ewatanabe786@gmail.com>
2023-06-17 21:34:56 +0900

    backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import { useState } from "react";
-import Form from "./components/Form";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data));
+  }, []);
+
   return (
-    <div>
-      <Form />
-    </div>
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
   );
 }
 
