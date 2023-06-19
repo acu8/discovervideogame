e2f98d545101462dab5e293d9bdc02d9ee1265b2
acu8 <ewatanabe786@gmail.com>
2023-06-19 21:24:38 +0900

    game card

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { HStack, Image } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 
-interface Props {
-  cartItemsCount: number;
-}
-
-const NavBar = ({ cartItemsCount }: Props) => {
-  return <div>NavBar: {cartItemsCount}</div>;
+const NavBar = () => {
+  return (
+    <HStack justifyContent="space-between" padding="10px">
+      <Image src={logo} boxSize="60px" />
+      <ColorModeSwitch />
+    </HStack>
+  );
 };
 
 export default NavBar;
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,11 @@
 import axios, { CanceledError } from 'axios';
 
 export default axios.create({
-    baseURL: '"https://jsonplaceholder.typicode.com/',
+    baseURL: 'https://api.rawg.io/api',
+    params: {
+        key: 'b4e76b0f7c754489b6309e5e17649063'
+    }
  
 })
 
-export { CanceledError };
+
