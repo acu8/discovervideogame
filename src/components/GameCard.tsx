b427eb9d258a9b10de06ae8320f06cf50bd015e8
acu8 <ewatanabe786@gmail.com>
2023-06-20 22:01:15 +0900

    img

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Game } from "../hooks/useGames";
-import { Card, Image, CardBody, Heading } from "@chakra-ui/react";
+import { Card, Image, CardBody, Heading, Text, HStack } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
+import Criticscore from "./Criticscore";
+import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   game: Game;
@@ -9,9 +12,16 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
+        <HStack justifyContent="space-between">
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          s
+          <Criticscore score={game.metacritic} />
+        </HStack>
       </CardBody>
     </Card>
   );
